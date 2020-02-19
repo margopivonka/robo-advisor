2260a99f88a6d003bc2c632539aa8ab7ecab2d86
margopivonka <59657652+margopivonka@users.noreply.github.com>
2020-02-19 11:27:39 -0500

    What We did in class

--- App/robo_advisor.py
+++ App/robo_advisor.py
@@ -1,7 +1,63 @@
 # app/robo_advisor.py
 
+import requests
+import json
+import os
+from dotenv import load_dotenv
+
+load_dotenv()
+
+
+API_KEY = os.getenv("ALPHAVANTAGE_API_KEY", default="OOPS")
+#SYMBOL = "TSLA"
+SYMBOL =input("Please enter a company NYSE symbol: ")
+
+request_url = f"https://www.alphavantage.co/query?function=TIME_SERIES_INTRADAY&symbol={SYMBOL}&interval=5min&outputsize=full&apikey={API_KEY}"
+print("URL: ", request_url)
+
+#request_url = f"https://www.alphavantage.co/query?function=TIME_SERIES_DAILY&symbol=TSLA&apikey={API_KEY}"
+
+#SYMBOL = input("Enter a company's symbol: ")
+
+response = requests.get(request_url)
+print(type(response))
+print(response.status_code)
+print(type(response.text))
+
+## FIGURE OUT HOW TO LOOP THIS SO IF A USER ENTERS AN INVALID SYMBOL THEY JUST TRY AGAIN
+while True:
+    SYMBOL =input("Please enter a company NYSE symbol: ")
+    if "Error Message" in response.text:
+    print("Oops, couldn't find that symbol, please try a new one.")
+        break
+   
+    else:
+        selected_ids.append(selected_id)
+
+parsed_response = json.loads(response.text)
+print(type(parsed_response))
+#print(parsed_response)
+
+
+
+
+#breakpoint()
+
+##ADD TO READ.ME HOW TO SETUP API KEY
+
+
+
+
+
+
+
+
+
+
+
+
 print("-------------------------")
-print("SELECTED SYMBOL: XYZ")
+print("SELECTED SYMBOL: ", SYMBOL)
 print("-------------------------")
 print("REQUESTING STOCK MARKET DATA...")
 print("REQUEST AT: 2018-02-20 02:00pm")
